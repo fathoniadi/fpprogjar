c53855c76f19d74408685ec3210e7acab1104fae
aninditalarasati <aninditalarasati@yahoo.com>
2016-12-15 23:06:03 +0700

    menu

---
+++ menu.py
@@ -0,0 +1,27 @@
+import pygame
+import Bomb
+import GameMap
+import Player
+import State
+import socket
+import threading
+import select
+
+class Menu():
+    def __init__(self):
+        pass
+        pygame.init()
+        width, height = 800, 600
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("BomberMan")
+        self.initSprite()
+
+    def initSprite(self):
+        self.menu = pygame.transform.scale(pygame.image.load("./assets/background_ready&start.png"), (800, 600))
+        self.screen.blit(self.menu,(0,0))
+
+    def update(self):
+        pygame.display.flip()
+            
+menu=Menu()
+menu.update()
