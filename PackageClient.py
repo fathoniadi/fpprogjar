02bc4f106c86ecd9ca384e129649c28c5a48ff9c
aninditalarasati <aninditalarasati@yahoo.com>
2016-12-20 14:36:03 +0700

    edit PackageClient

--- PackageClient.py
+++ PackageClient.py
@@ -6,36 +6,44 @@ class PackageClient():
         # super(Package, self).__init__()
         pass
 
+    #fungsi untuk create game
     def createGame(self,_room):
         self.data={'code':1,'room':_room}
         return self.data
 
+    #fungsi untuk menghubungkan ke room client-server
     def connectToRoom(self,_room):
         self.data = {'code': 2, 'room': _room}
         return self.data
 
+    #fungsi inisialisasi game
     def startGame(self,_room):
         self.data = {'code':100,'room':_room}
         return self.data
 
+    #menyimpan lokasi
     def location(self,_x,_y,_player,_room):
         self.data = {'code':200,'playerName':_player,'x':_x,'y':_y,'room':_room}
         return self.data
 
+    #apabila player terkena bomb
     def createPackagePlayerDead(self, _playerName):
         self.data = {'code': 300, 'playerName': _playerName}
         return self.data
 
+    #membuat bomob pada lokasi (x,y)
     def createPackageBomb(self,_x,_y,_room):
         self.data = {'code':201, 'room':_room,'x':_x,'y':_y}
         return self.data
-
+    
+    #
     def deSerialization(self, data):
         return json.loads(data)
 
     def serialization(self, data):
         return json.dumps(data)
 
+    #fungsi untuk mengirim lokasi bomb dari player satu ke player lainnya
     def SendNewLocBomb(self, _playerName, room):
         self.data = {'code': 201, 'playerName': _playerName, 'room': _room}
         return self.data
